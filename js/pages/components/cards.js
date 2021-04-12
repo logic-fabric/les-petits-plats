984e218e2eb1582c201bee5e9f847e9b4a53af68
logic-fabric <lowic@logic-fabric.fr>
2021-04-12 22:06:19 +0200

    refactor: cards.js

--- js/pages/components/cards.js
+++ js/pages/components/cards.js
@@ -15,39 +15,43 @@ export class RecipeCard {
           : `&nbsp;: ${ingredient.quantity}`
         : "";
 
-      htmlContent += `<p>
-                      <strong>
-                        ${ingredient.ingredient}
-                      </strong> ${ingredientQuantity}
-                    </p>`;
+      htmlContent += (
+        `<p>
+          <strong>
+            ${ingredient.ingredient}
+          </strong> ${ingredientQuantity}
+        </p>`
+      );
     }
 
     return htmlContent;
   }
 
   get html() {
-    return `<article class="c-card lg4 md6 sm12">
-              <div class="c-card__img"></div>
+    return (
+      `<article class="c-card lg4 md6 sm12">
+        <div class="c-card__img"></div>
 
-              <div class="c-card__body">
-                <h2 class="c-card__title">
-                  <span>${this._recipe.name}</span>
-                  <span>
-                    <i class="far fa-clock"></i>
-                    ${this._recipe.time} min
-                  </span>
-                </h2>
+        <div class="c-card__body">
+          <h2 class="c-card__title">
+            <span>${this._recipe.name}</span>
+            <span>
+              <i class="far fa-clock"></i>
+              ${this._recipe.time} min
+            </span>
+          </h2>
 
-                <div class="c-card__recipe row-12 has-gutter-lg">
-                  <div class="c-card__ingredients lg6 md6 sm6">
-                    ${this._ingredientsHtml()}
-                  </div>
+          <div class="c-card__recipe row-12 has-gutter-lg">
+            <div class="c-card__ingredients lg6 md6 sm6">
+              ${this._ingredientsHtml()}
+            </div>
 
-                  <div class="c-card__description lg6 md6 sm6">
-                    <p>${this._recipe.description}</p>
-                  </div>
-                </div>
-              </div>
-            </article>`;
+            <div class="c-card__description lg6 md6 sm6">
+              <p>${this._recipe.description}</p>
+            </div>
+          </div>
+        </div>
+      </article>`
+    );
   }
 }
