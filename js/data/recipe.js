81c2ea47e0edd2cc515b53b456eda79d390aa375
logic-fabric <lowic@logic-fabric.fr>
2021-04-12 22:02:51 +0200

    refactor: RecipesList._sortByName() now use sortAlphabetically() utility function

--- js/data/recipe.js
+++ js/data/recipe.js
@@ -64,7 +64,7 @@ export class Recipe {
 export class RecipesList {
   constructor(recipes) {
     this.recipes = recipes;
-    this.sortByName();
+    this._sortByName();
   }
 
   get sortedAppliances() {
@@ -77,6 +77,19 @@ export class RecipesList {
 
   get sortedUstensils() {
     return sortAlphabetically(this._collectUstensils());
+  }
+
+  _sortByName() {
+    return this.recipes.sort((r1, r2) => {
+      const name1 = r1.name.toLowerCase();
+      const name2 = r2.name.toLowerCase();
+
+      const [sortedName1, sortedName2] = sortAlphabetically([name1, name2]);
+
+      if (sortedName1 === name2) return 1;
+      if (sortedName1 === name1) return -1;
+      return 0;
+    });
   }
 
   _collectAppliances() {
@@ -159,16 +172,5 @@ export class RecipesList {
     }
 
     return new RecipesList([...filteredRecipes]);
-  }
-
-  sortByName() {
-    return this.recipes.sort((r1, r2) => {
-      const name1 = r1.name.toLowerCase();
-      const name2 = r2.name.toLowerCase();
-
-      if (name1 > name2) return 1;
-      if (name1 < name2) return -1;
-      return 0;
-    });
   }
 }
