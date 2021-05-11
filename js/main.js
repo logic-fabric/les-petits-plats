49766602ae7030bed58d22dfa0f4cb2dd2062c21
logic-fabric <lowic@logic-fabric.fr>
2021-05-11 09:41:07 +0200

    refactor: main.js

--- js/main.js
+++ js/main.js
@@ -9,33 +9,36 @@ const recipesList = dataFetcher.getRecipesList();
 
 new HomePageBuilder(recipesList).render();
 
-/* ==================================
-   Search algorithm peformance tester
-   ================================== */
+// SEARCH ALGORITHM PERFORMANCE:
+measureAlgorithmPerformance();
 
-// with a request corresponding to 4 recipes:
-const TEST_USER_REQUEST_1 = {
-  userInput: "choco",
-  joinedBadges: "sucre beurre",
-};
-// with a request corresponding to no recipe:
-const TEST_USER_REQUEST_2 = {
-  userInput: "choco",
-  joinedBadges: "maïs basilic",
-};
-const TESTS_QUANTITY = 1_000;
+function measureAlgorithmPerformance() {
+  // with a request corresponding to 4 recipes:
+  const TEST_USER_REQUEST_1 = {
+    userInput: "choco",
+    joinedBadges: "sucre beurre",
+  };
+  // with a request corresponding to no recipe:
+  const TEST_USER_REQUEST_2 = {
+    userInput: "choco",
+    joinedBadges: "maïs basilic",
+  };
+  const TESTS_QUANTITY = 1_000;
 
-const testStarting = Date.now();
+  const testStarting = Date.now();
 
-let searchResult1, searchResult2;
+  let searchResult1, searchResult2;
 
-for (let _ = 0; _ < TESTS_QUANTITY; _++) {
-  searchResult1 = recipesList.search(TEST_USER_REQUEST_1);
-  searchResult2 = recipesList.search(TEST_USER_REQUEST_2);
+  for (let _ = 0; _ < TESTS_QUANTITY; _++) {
+    searchResult1 = recipesList.search(TEST_USER_REQUEST_1);
+    searchResult2 = recipesList.search(TEST_USER_REQUEST_2);
+  }
+
+  const testEnding = Date.now();
+
+  const testDuration = testEnding - testStarting;
+
+  console.log(
+    `${2 * TESTS_QUANTITY} recherches réalisées en ${testDuration} ms`
+  );
 }
-
-const testEnding = Date.now();
-
-const testDuration = testEnding - testStarting;
-
-console.log(`${2 * TESTS_QUANTITY} recherches réalisées en ${testDuration} ms`);
