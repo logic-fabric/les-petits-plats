8f3d048de853a2b30454fb664c663ce047da5341
logic-fabric <lowic@logic-fabric.fr>
2021-10-12 18:32:36 +0200

    refactor: decompose buildHashTableForSearchingRecipes() in 3 functions to lower code complexity

--- js/data/dataFetcher.js
+++ js/data/dataFetcher.js
@@ -39,32 +39,63 @@ export class DataFetcher {
   }
 }
 
+/* ================================
+   HASH TABLE FOR SEARCHING RECIPES
+   ================================ */
+
+/**
+ * @param {Recipe} recipe
+ * @returns {Array.string}
+ */
+function extractKeywordsFromRecipe(recipe) {
+  let recipeWords = `${recipe.nameWithoutAccent} ${recipe.joinedIngredientsWithoutAccent} ${recipe.applianceNameWithoutAccent} ${recipe.joinedUstensilsWithoutAccent} ${recipe.descriptionWithoutAccent}`;
+
+  recipeWords = recipeWords.split(" ");
+
+  const recipeKeywords = removeStopWords(recipeWords);
+
+  return recipeKeywords;
+}
+
+/**
+ * Complete the hash table for a pre-treated recipe.
+ * @param {Recipe} recipe
+ * @param {Array.string} recipeKeywords
+ * @param {Object} hashTable
+ * @returns
+ */
+function addRecipeKeywordsToHashTable(recipe, recipeKeywords, hashTable) {
+  for (let keyword of recipeKeywords) {
+    for (let i = 1; i <= keyword.length; i++) {
+      const troncatedKeyword = keyword.slice(0, i);
+
+      if (troncatedKeyword in hashTable) {
+        hashTable[troncatedKeyword].add(recipe);
+      } else {
+        hashTable[troncatedKeyword] = new Set([recipe]);
+      }
+    }
+  }
+
+  return hashTable;
+}
+
 /**
  * Return an object associating all potential matching keyword with the set of recipes containing this keyword.
  * @param {RecipesList} recipesList
  * @returns {Object}
  */
 export function buildHashTableForSearchingRecipes(recipesList) {
-  const hashTableForSearchingRecipes = {};
+  let hashTableForSearchingRecipes = {};
 
   for (let recipe of recipesList.recipes) {
-    let recipeWords = `${recipe.nameWithoutAccent} ${recipe.joinedIngredientsWithoutAccent} ${recipe.applianceNameWithoutAccent} ${recipe.joinedUstensilsWithoutAccent} ${recipe.descriptionWithoutAccent}`;
+    const recipeKeywords = extractKeywordsFromRecipe(recipe);
 
-    recipeWords = recipeWords.split(" ");
-
-    const recipeKeywords = removeStopWords(recipeWords);
-
-    for (let keyword of recipeKeywords) {
-      for (let i = 1; i <= keyword.length; i++) {
-        const troncatedKeyword = keyword.slice(0, i);
-
-        if (troncatedKeyword in hashTableForSearchingRecipes) {
-          hashTableForSearchingRecipes[troncatedKeyword].add(recipe);
-        } else {
-          hashTableForSearchingRecipes[troncatedKeyword] = new Set([recipe]);
-        }
-      }
-    }
+    hashTableForSearchingRecipes = addRecipeKeywordsToHashTable(
+      recipe,
+      recipeKeywords,
+      hashTableForSearchingRecipes
+    );
   }
 
   return hashTableForSearchingRecipes;
